95abbbce66950cf8233917a4ca4f4223e5a9b539
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2016-11-07 08:27:34 +0200

    AddData MxResult extension

---
+++ Maxxor.PCL/MxResults/MxResultExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Maxxor.PCL.MxResults
+{
+    public static class MxResultExtensions
+    {
+        public static MxResult AddData(this MxResult result, string key, string value)
+        {
+            result.Error.AddData(key, value);
+            return result;
+        }
+
+        public static MxResult<T> AddData<T>(this MxResult<T> result, string key, string value)
+        {
+            result.Error.AddData(key, value);
+            return result;
+        }
+    }
+}
