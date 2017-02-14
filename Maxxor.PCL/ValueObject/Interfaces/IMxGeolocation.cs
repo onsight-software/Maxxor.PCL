fe1e382233caf2b7fa9e22f00f36e63b111d8452
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2017-02-14 13:24:14 +0200

    first commit of MxGeolocation

---
+++ Maxxor.PCL/ValueObject/Interfaces/IMxGeolocation.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace Maxxor.PCL.ValueObject.Interfaces
+{
+    public interface IMxGeolocation
+    {
+        IMxGpsCoordinates GpsCoordinates { get; }
+        double Accuracy { get; }
+        double Altitude { get; }
+        double Heading { get; }
+        double Speed { get; }
+        DateTimeOffset DateTimeOffset { get;  }
+    }
+}
---
+++ Maxxor.PCL/ValueObject/MxGeolocation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Maxxor.PCL.ValueObject.Base;
+using Maxxor.PCL.ValueObject.Interfaces;
+
+namespace Maxxor.PCL.ValueObject
+{
+    public class MxGeolocation : MxValueObject<MxGeolocation>, IMxGeolocation
+    {
+        public MxGeolocation(IMxGpsCoordinates gpsCoordinates, double accuracy, double altitude, double heading, double speed, DateTimeOffset dateTimeOffset)
+        {
+            GpsCoordinates = gpsCoordinates;
+            Accuracy = accuracy;
+            Altitude = altitude;
+            Heading = heading;
+            Speed = speed;
+            DateTimeOffset = dateTimeOffset;
+        }
+
+        public IMxGpsCoordinates GpsCoordinates { get; }
+        public double Accuracy { get; }
+        public double Altitude { get; }
+        public double Heading { get; }
+        public double Speed { get; }
+        public DateTimeOffset DateTimeOffset { get; }
+    }
+}
