960554c0ae28b938cdba5909ae51d029408ab57c
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2017-02-14 15:08:42 +0200

    Feature/mx geo location (#19)
    * first commit of MxGeolocation
    
    * slimmed it down based on what we get from the plugin

---
+++ Maxxor.PCL/ValueObject/Interfaces/IMxGeolocation.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace Maxxor.PCL.ValueObject.Interfaces
+{
+    public interface IMxGeolocation
+    {
+        IMxGpsCoordinates GpsCoordinates { get; }
+        double Accuracy { get; }
+        DateTimeOffset DateTimeOffset { get;  }
+    }
+}
---
+++ Maxxor.PCL/ValueObject/MxGeolocation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Maxxor.PCL.ValueObject.Base;
+using Maxxor.PCL.ValueObject.Interfaces;
+
+namespace Maxxor.PCL.ValueObject
+{
+    public class MxGeolocation : MxValueObject<MxGeolocation>, IMxGeolocation
+    {
+        public MxGeolocation(IMxGpsCoordinates gpsCoordinates, double accuracy, DateTimeOffset dateTimeOffset)
+        {
+            GpsCoordinates = gpsCoordinates;
+            Accuracy = accuracy;
+            DateTimeOffset = dateTimeOffset;
+        }
+
+        public IMxGpsCoordinates GpsCoordinates { get; }
+        public double Accuracy { get; }
+        public DateTimeOffset DateTimeOffset { get; }
+    }
+}
