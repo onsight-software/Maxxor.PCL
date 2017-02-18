dac8d878ac4de445d7fda64e4bf6f7de265bd440
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2017-02-18 12:06:23 +0200

    Feature/asset type fix (#20)
    * Added a simple MxEnum - a value object with one field
    
    * no message
    
    * no message

---
+++ Maxxor.PCL.Tests/Tests/MxValueObjectsTests/MxEnumTests/ToStringTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Maxxor.PCL.Tests.Tests.Base;
+using Maxxor.PCL.ValueObject;
+using Maxxor.PCL.ValueObject.Base;
+using NUnit.Framework;
+
+namespace Maxxor.PCL.Tests.Tests.MxValueObjectsTests.MxEnumTests
+{
+    [TestFixture, ExcludeFromCodeCoverage]
+    public class ToStringTests : BaseUnitTest
+    {
+
+        [Test]
+        public void Objects_with_same_value_SHOULD_be_Equal()
+        {
+            //Arrange
+            var sut = MyEnum.FirstValue;
+
+            //Act
+            var result = sut.ToString();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("FirstValue"));
+        }
+
+        private class MyEnum : MxEnum<MyEnum>
+        {
+            public MyEnum(string typeName) : base(typeName)
+            {
+            }
+
+            public static readonly MyEnum FirstValue =  new MyEnum("FirstValue");
+        }
+    }
+}
---
+++ Maxxor.PCL/ValueObject/Base/MxEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace Maxxor.PCL.ValueObject.Base
+{
+    public abstract class MxEnum<T> : MxValueObject<MxEnum<T>>
+    {
+        protected readonly string Name;
+
+        protected MxEnum(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
