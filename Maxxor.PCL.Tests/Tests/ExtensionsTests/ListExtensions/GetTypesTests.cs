a34125b836a7bbdd7d72993ce886079c9c791807
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2017-04-02 10:15:33 +0200

    Added GetTypes extension method

---
+++ Maxxor.PCL.Tests/Tests/ExtensionsTests/ListExtensions/GetTypesTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Maxxor.PCL.Extensions;
+using Maxxor.PCL.Result;
+using Maxxor.PCL.Tests.Tests.Base;
+using NUnit.Framework;
+
+namespace Maxxor.PCL.Tests.Tests.ExtensionsTests.ListExtensions
+{
+    [TestFixture]
+    public class GetTypesTests : BaseUnitTest
+    {
+        [Test]
+        public void SHOULD_return_list_of_types()
+        {
+            //Arrange
+            var sut = new List<object>
+            {
+                "hello",
+                MxErrorCondition.Unspecified
+            };
+
+            //Act
+            var result = sut.GetTypes();
+
+            //Assert
+            Assert.That(result[0], Is.EqualTo(typeof(string)));
+            Assert.That(result[1], Is.EqualTo(typeof(MxErrorCondition)));
+
+        }
+
+    }
+}
---
+++ Maxxor.PCL/Extensions/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maxxor.PCL.Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<Type> GetTypes(this List<object> objects)
+        {
+            var types = new List<Type>();
+            foreach (var obj in objects)
+            {
+                types.Add(obj.GetType());
+            }
+            return types;
+        }
+    }
+}
