33760976c7cd33235bfee12f3b23bdfccecccaa3
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2017-06-08 12:15:56 +0200

    added some handy statics for common MxCurrency values

--- Maxxor.PCL/ValueObject/MxCurrency.cs
+++ Maxxor.PCL/ValueObject/MxCurrency.cs
@@ -1,10 +1,12 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using System.Resources;
 using Maxxor.PCL.ValueObject.Base;
 using Maxxor.PCL.ValueObject.Interfaces;
 
 namespace Maxxor.PCL.ValueObject
 {
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class MxCurrency :  MxValueObject<MxCurrency>, IMxCurrency
     {
         public MxCurrency(string currencyCode)
@@ -21,5 +23,11 @@ namespace Maxxor.PCL.ValueObject
 
         public string CurrencyCode { get; }
         public string CurrencySymbol { get; private set; } = string.Empty;
+
+        public static MxCurrency ZAR = new MxCurrency(nameof(ZAR));
+        public static MxCurrency AUD = new MxCurrency(nameof(AUD));
+        public static MxCurrency GBP = new MxCurrency(nameof(GBP));
+        public static MxCurrency USD = new MxCurrency(nameof(USD));
+        public static MxCurrency CAD = new MxCurrency(nameof(CAD));
     }
 }
