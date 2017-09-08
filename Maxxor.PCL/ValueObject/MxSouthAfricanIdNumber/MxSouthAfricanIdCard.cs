c59132337d700f9fcdc3fda21fa99880c2ac4fd9
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2017-09-08 12:32:08 +0200

    no message

---
+++ Maxxor.PCL/ValueObject/MxSouthAfricanIdNumber/MxSouthAfricanIdCard.cs
@@ -0,0 +1,34 @@
+﻿using Maxxor.PCL.Result;
+using Maxxor.PCL.ValueObject.Base;
+
+namespace Maxxor.PCL.ValueObject.MxSouthAfricanIdNumber
+{
+    public class MxSouthAfricanIdCard : MxValueObject<MxSouthAfricanIdCard>
+    {
+
+        public MxSouthAfricanIdCard()
+        {
+            IdNumber = new MxSouthAfricanIdNumber();
+        }
+
+        public MxSouthAfricanIdNumber IdNumber { get; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public MxResult<MxSouthAfricanIdCard> Populate(string scannedString)
+        {
+            var strings = scannedString.Split('|');
+
+            var idNumberResult = IdNumber.Populate(strings[4]);
+            if (idNumberResult.IsFailure)
+            {
+                return MxResult.Fail<MxSouthAfricanIdCard>(this, idNumberResult);
+            }
+
+            LastName = strings[0];
+            FirstName = strings[1];
+
+            return MxResult.Ok(this);
+        }
+    }
+}
