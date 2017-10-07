af1ee70718a8c4c77a2f485f65a93c83884fc3de
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2017-10-07 12:49:47 +0200

    MxSparseMatrix (#35)

---
+++ Maxxor.PCL.Tests/Tests/CollectionTests/SparseMatrixTests/GetAtTests.cs
@@ -0,0 +1,38 @@
+﻿using Maxxor.PCL.Collections;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Maxxor.PCL.Tests.Tests.CollectionTests.SparseMatrixTests
+{
+    [TestFixture]
+    public class SetAtTests
+    {
+
+        [Test]
+        public void SHOULD_set_value_at_xy_position()
+        {
+            //Arrange
+            var sut = new MxSparseMatrix<string>();
+
+            //Act
+            sut.SetAt(10, 15, "me");
+
+            //Assert
+            Assert.That(sut[10, 15], Is.EqualTo("me"));
+        }
+
+        [Test]
+        public void SHOULD_replace_value_at_xy_position()
+        {
+            //Arrange
+            var sut = new MxSparseMatrix<string>();
+            sut.SetAt(10, 15, "me");
+
+            //Act
+            sut.SetAt(10, 15, "you");
+
+            //Assert
+            Assert.That(sut[10, 15], Is.EqualTo("you"));
+        }
+    }
+}
